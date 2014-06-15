7734247f42e8c0ffba3f8fda8c85000e25f75a03
Mikel Pintor <millen@gmail.com>
2014-06-15 12:40:38 +0200

    Modificado el ejemplo de Mamdani para utilizar la función get_x().

--- inferencia/fire_detection/fire_detection_mamdani_example.m
+++ inferencia/fire_detection/fire_detection_mamdani_example.m
@@ -3,16 +3,9 @@ addpath('./lang_variables');
 addpath('../functions');
 addpath('../matlab2tikz');
 
-x_threat = 0:100;
+x_threat = threat.get_x();
 
-rules = fire_detection_rules();
-[m,n] = size(rules);
-%Inicializar el conjunto de reglas
-for i=1:m
-    R(i).n = i;
-    R(i).A = rules(i,1:n-1);
-    R(i).B = rules{i,n};
-end
+R = fire_detection_rules();
 fact = [30,30,1000,50,40];
 y.v(1,:) = x_threat;
 
