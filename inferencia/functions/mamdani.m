bf1c99268cec6189afdd2b8ef9c4071866eae000
Mikel Pintor <millen@gmail.com>
2014-06-21 23:24:15 +0200

    Añadidos comentarios.

--- inferencia/functions/mamdani.m
+++ inferencia/functions/mamdani.m
@@ -1,3 +1,13 @@
+% Método de Mamdani.
+% Parámetros:
+% R: Conjunto de reglas.
+% fact: Premisa. Vector de valores de entradas escalares. El valor para el
+% antecedente i-ésimo debe estar en la posición fact(i).
+% y: Universo de salida
+%
+% Devuelve:
+% B = Conjunto de salida.
+
 function B = mamdani( R, fact, y)
 
 n = length(R);
@@ -5,18 +15,18 @@ B = zeros(length(R), length(y));
 
 % Evaluar cada regla
 for i=1:n
-  %Evaluar cada antecedente
+  % Evaluar cada antecedente
   u = zeros(1,length(R(i).A));
   for j=1:length(R(i).A)
       u(j) = R(i).A{j}(fact(j));
   end
   
-  %Obtener el valor del fuzzy set de salida para esta regla "clippeando" el consecuente con
-  %el mínimo de los grados de pertenencia de cada antecedente.
+  % Obtener el valor del conjunto difuso de salida para esta regla truncando el consecuente con
+  % el mínimo de los grados de pertenencia de cada antecedente.
   B(i,:) = min(arrayfun(R(i).B, y),min(u)); 
 end
 
-%Agregar todos los consecuentes obtenidos utilizando el máximo
+% Obtener el conjunto de salida agregando todos los consecuentes obtenidos utilizando el máximo
 B = max(B,[],1);
-end
 
+end
