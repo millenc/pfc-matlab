1d980507ba1be81a72366cdb50922bff705082b0
Mikel Pintor <millen@gmail.com>
2014-06-22 16:44:14 +0200

    Añadidos parámetros opcionales a las funciones *_plot() para controlar si se dibujan el título, la leyenda o se genera el archivo .tikz. Modificaciones en los ejemplos para utilizar estos parámetros.

--- inferencia/fire_detection/fire_detection_interpolation_plot.m
+++ inferencia/fire_detection/fire_detection_interpolation_plot.m
@@ -1,5 +1,9 @@
-function [B, dc, db, dm, ds, dl] = fire_detection_interpolation_plot( O, T, M, t, s, l, h, d )
+function [B, dc, db, dm, ds, dl] = fire_detection_interpolation_plot( O, T, M, t, s, l, h, d, varargin )
 addpath('../matlab2tikz');
+
+% Parser de parámetros opcionales
+p = fire_detection_plot_input_parser();
+parse(p,varargin{:});
 
 % Obtener los resultados con el método de interpolación
 [B, dc, db, dm, ds, dl] = fire_detection_interpolation(O.f, T, M, t, s, l, h, d);
@@ -21,8 +25,14 @@ hds = plot(ds,B(ds+1),'vc','LineWidth',lw,'MarkerSize',msz);
 hdl = plot(dl,B(dl+1),'^m','LineWidth',lw,'MarkerSize',msz);
 hasbehavior(hl,'legend',false);
 xlabel('Riesgo (%)');
-%title(strcat('Temp: ',sprintf('%d',t),', Humo: ',sprintf('%d',s),', Luz: ',sprintf('%d',l),', Humedad: ',sprintf('%d',h),', Distancia: ',sprintf('%d',d)));
-%legend(strcat('centroid: ',sprintf('%d',dc)),strcat('bisector: ',sprintf('%d',db)),strcat('mom: ',sprintf('%d',dm)),strcat('som: ',sprintf('%d',ds)),strcat('lom: ',sprintf('%d',dl)));
-matlab2tikz(strcat('./output/interpolation/interpolation-','O-',O.name,'_T',func2str(T),'_M',func2str(M),'--','T',sprintf('%d',t),'_S',sprintf('%d',s),'_L',sprintf('%d',l),'_H',sprintf('%d',h),'_D',sprintf('%d',d),'.tikz'),'showInfo', false,'standalone', false,'height', '\figureheight', 'width', '\figurewidth');
+if(p.Results.showTitle == true)
+    title(strcat('Temp: ',sprintf('%d',t),', Humo: ',sprintf('%d',s),', Luz: ',sprintf('%d',l),', Humedad: ',sprintf('%d',h),', Distancia: ',sprintf('%d',d)));
+end
+if(p.Results.showLegend == true)
+    legend(strcat('centroid: ',sprintf('%d',dc)),strcat('bisector: ',sprintf('%d',db)),strcat('mom: ',sprintf('%d',dm)),strcat('som: ',sprintf('%d',ds)),strcat('lom: ',sprintf('%d',dl)));
+end
+if(p.Results.exportTikz == true)
+    matlab2tikz(strcat('./output/interpolation/interpolation-','O-',O.name,'_T',func2str(T),'_M',func2str(M),'--','T',sprintf('%d',t),'_S',sprintf('%d',s),'_L',sprintf('%d',l),'_H',sprintf('%d',h),'_D',sprintf('%d',d),'.tikz'),'showInfo', false,'standalone', false,'height', '\figureheight', 'width', '\figurewidth');
 end
 
+end
